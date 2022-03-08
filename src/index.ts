cb18a60157be6d7b876b707ee49352f7020a8363
Kylie <kylie@comets.kr>
2022-03-08 11:43:10 +0900

    Creating a Block with Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age:number, gender?:string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+import * as CryptoJS from "crypto-js";
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number, 
+    previousHash: string, 
+    timestamp: number, 
+    data: string
+    ):string => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const kylie = new Human("Kylie", 29, "female")
+const genesisBlock: Block = new Block(0," 202203080943", "", "Hello", 123456);
+let blockchain: Block[] = [genesisBlock];
+const getBlockchain = () : Block[] => blockchain;
+const getLatestBlock = () : Block => blockchain[blockchain.length -1];
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
-
-const result =sayHi(kylie);
-console.log(result)
 export {}
