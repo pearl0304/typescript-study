0090227456b140f379576799e6856df74febe847
Kylie <kylie@comets.kr>
2022-03-08 09:37:36 +0900

    Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-interface Human {
-  name: string,
-  age: number,
-  gender: string
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age:number, gender?:string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
-const person = {
-  name: "Kylie",
-  age: 29,
-  gender: "female"
-};
+
+const kylie = new Human("Kylie", 29, "female")
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-const result =sayHi(person);
+const result =sayHi(kylie);
 console.log(result)
 export {}
