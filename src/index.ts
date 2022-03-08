b6179b2d928306ed663e27ac32bfa7603a3379ef
Kylie <kylie@comets.kr>
2022-03-08 09:29:51 +0900

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name: string,
+  age: number,
+  gender: string
 }
+const person = {
+  name: "Kylie",
+  age: 29,
+  gender: "female"
+};
 
-const result =sayHi("kylie", 29, "female");
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+const result =sayHi(person);
 console.log(result)
 export {}
